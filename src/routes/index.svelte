242f072d31a1ba8dfd996d6f055e9a3b7930abd3
jpaquim <joaopaquim@gmail.com>
2022-03-15 12:03:41 +0000

    Remove experimental-webgl context support

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -201,37 +201,16 @@
 				preserveDrawingBuffer: false
 			};
 
-			let gl = canvas.getContext('webgl2', params);
-			const isWebGL2 = !!gl;
-			if (!isWebGL2)
-				gl = canvas.getContext('webgl', params) || canvas.getContext('experimental-webgl', params);
-
-			let halfFloat;
-			let supportLinearFiltering;
-			if (isWebGL2) {
-				gl.getExtension('EXT_color_buffer_float');
-				supportLinearFiltering = gl.getExtension('OES_texture_float_linear');
-			} else {
-				halfFloat = gl.getExtension('OES_texture_half_float');
-				supportLinearFiltering = gl.getExtension('OES_texture_half_float_linear');
-			}
+			const gl = canvas.getContext('webgl2', params);
+			gl.getExtension('EXT_color_buffer_float');
+			const supportLinearFiltering = gl.getExtension('OES_texture_float_linear');
 
 			gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
-			const halfFloatTexType = isWebGL2 ? gl.HALF_FLOAT : halfFloat.HALF_FLOAT_OES;
-			let formatRGBA;
-			let formatRG;
-			let formatR;
-
-			if (isWebGL2) {
-				formatRGBA = getSupportedFormat(gl, gl.RGBA16F, gl.RGBA, halfFloatTexType);
-				formatRG = getSupportedFormat(gl, gl.RG16F, gl.RG, halfFloatTexType);
-				formatR = getSupportedFormat(gl, gl.R16F, gl.RED, halfFloatTexType);
-			} else {
-				formatRGBA = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType);
-				formatRG = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType);
-				formatR = getSupportedFormat(gl, gl.RGBA, gl.RGBA, halfFloatTexType);
-			}
+			const halfFloatTexType = gl.HALF_FLOAT;
+			const formatRGBA = getSupportedFormat(gl, gl.RGBA16F, gl.RGBA, halfFloatTexType);
+			const formatRG = getSupportedFormat(gl, gl.RG16F, gl.RG, halfFloatTexType);
+			const formatR = getSupportedFormat(gl, gl.R16F, gl.RED, halfFloatTexType);
 
 			return {
 				gl,
